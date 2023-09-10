9420e80f7c47d6e640c63249beca0e600b1ffed7
AnthonyPowell <anthonydewpow@gmail.com>
2023-09-10 17:22:40 -0500

    commit tree bca18a63284e46a07ea2a88fe73bcd06d05e4893

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
+function Movies({ movies: { title, time, genres } }) {
+  const MovieCard = movies.map((movies) => {
+    return;
+  });
+  return (
+    <li>
+      <ul className="cards">{MovieCard}</ul>
+    </li>
+  );
 }
 
 export default Movies;
